d6dbae25555096b6e0f79fb145b37ccf40237a09
atijuhasz <a.juhasz94@gmail.com>
2026-01-17 12:51:20 +0100

    Update exploratory_analysis.R

--- scripts/exploratory_analysis.R
+++ scripts/exploratory_analysis.R
@@ -1,9 +1,11 @@
 source("utils/load_packages.R")
 source("functions/read_multiple_csvs.R")
 source("functions/clean_basic.R")
+source("functions/summary_helper.R")
 
 raw_data <- read_multiple_csvs("data/raw")
-
 clean_data <- clean_basic(raw_data)
 
 glimpse(clean_data)
+
+summary_helper(clean_data)
